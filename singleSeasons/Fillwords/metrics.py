9cb641bf16433e007d86368f9e75df27e7e0d100
Richard <richardaudeacnt@gmail.com>
2019-09-14 17:24:15 +0200

    Staffelwerte einzeln

---
+++ singleSeasons/Fillwords/metrics.py
@@ -0,0 +1,180 @@
+import math
+from nltk import tokenize
+from nltk.tokenize import RegexpTokenizer
+
+def wahrscheinlichkeiten(txt):
+	properties = {}
+	summe = 0
+	for sign in txt:
+		summe += 1
+		if sign not in properties:
+			properties[sign] = 1 #nur auf den Zähler schauen
+		else: 
+			properties[sign] = properties[sign]+1
+	realProbs = {}
+	for k, v in properties.items(): # jetzt sind es die Wahrscheinlichkeiten, unter der Annahme, dass die Zeichen stochastisch unabhängig voneinander sind
+		realProbs[k] = v/summe
+	return realProbs
+	# vergleichbar mit Hmax in: https://de.wikipedia.org/wiki/Entropie_(Informationstheorie)
+	
+def englishProperties():
+	# Werte von hier: https://de.wikipedia.org/wiki/Entropie_(Kryptologie) - für Englisch
+	engProp = {}
+	engProp["a"] = 7.19/100
+	engProp["b"] = 1.58/100
+	engProp["c"] = 4.05/100
+	engProp["d"] = 3.11/100
+	engProp["e"] = 13.05/100
+	engProp["f"] = 2.42/100
+	engProp["g"] = 2.34/100
+	engProp["h"] = 4.71/100
+	engProp["i"] = 7.71/100
+	engProp["j"] = 0.09/100
+	engProp["k"] = 0.58/100
+	engProp["l"] = 3.72/100
+	engProp["m"] = 2.54/100
+	engProp["n"] = 7.81/100
+	engProp["o"] = 7.52/100
+	engProp["p"] = 2.3/100
+	engProp["q"] = 0.1/100
+	engProp["r"] = 6.41/100
+	engProp["s"] = 6.49/100
+	engProp["t"] = 9.22/100
+	engProp["u"] = 2.83/100
+	engProp["v"] = 0.86/100
+	engProp["w"] = 1.07/100
+	engProp["x"] = 0.45/100
+	engProp["y"] = 1.73/100
+	engProp["z"] = 0.1/100
+	return engProp
+	
+def deutschProps():
+	# für Deutsch - gleicher Variablenname für das Dictionary, da copypaste vom Englischen
+	engProp = {}
+	engProp["a"] = 5.45/100
+	engProp["b"] = 1.75/100
+	engProp["c"] = 3.37/100
+	engProp["d"] = 5.11/100
+	engProp["e"] = 16.89/100
+	engProp["f"] = 1.28/100
+	engProp["g"] = 3.76/100
+	engProp["h"] = 5.26/100
+	engProp["i"] = 8.51/100
+	engProp["j"] = 0.18/100
+	engProp["k"] = 1.51/100
+	engProp["l"] = 3.77/100
+	engProp["m"] = 2.22/100
+	engProp["n"] = 10.42/100
+	engProp["o"] = 3.11/100
+	engProp["p"] = 0.63/100
+	engProp["q"] = 0.01/100
+	engProp["r"] = 7.14/100
+	engProp["s"] = 6.24/100
+	engProp["t"] = 6.08/100
+	engProp["u"] = 3.4/100
+	engProp["v"] = 0.89/100
+	engProp["w"] = 1.64/100
+	engProp["x"] = 0.02/100
+	engProp["y"] = 0.07/100
+	engProp["z"] = 1.27/100
+	return engProp
+	
+def entropy(word):
+	word = word.lower()
+	negEntropy = 0
+	#alleP = wahrscheinlichkeiten(word)
+	alleP = englishProperties()
+	for s in word:
+		if s in alleP:
+		    val = alleP[s]
+		    negEntropy += val * math.log(val, 2)
+	entropy = 0 - negEntropy
+	return entropy
+	
+def maxEntropy(word):
+	word = word.lower()
+	negEntropy = 0
+	alleP = wahrscheinlichkeiten(word)
+	#alleP = englishProperties()
+	for s in word:
+		if s in alleP:
+		    val = alleP[s]
+		    negEntropy += val * math.log(val, 2)
+	entropy = 0 - negEntropy
+	return entropy
+	
+def token_type_stats(text): #dementia
+	num_tokens, num_types, known_types = 0, 0, set()
+	#tokens = text.split()
+	if len(text) > 0:
+		for word in text:
+			num_tokens += 1
+			#word = word.lower() so würde es noch kleiner werden
+			if not word in known_types:
+				known_types.add(word)
+				num_types += 1
+    #return (num_tokens, num_types, num_tokens / num_types)
+		return num_types/num_tokens
+	return 0    
+	
+def averageEntropy(txt):
+	words = txt.split()
+	averageEntropy = 0
+	if len(words) > 0:
+		totalEntropy = 0
+		for word in words:
+			totalEntropy += entropy(word)
+		averageEntropy = totalEntropy/len(words)
+	return averageEntropy
+	
+def averageRedundance(txt):
+	words = txt.split()
+	averageRedundance = 0
+	if len(words) > 0:
+		totalRedundance = 0
+		for word in words:
+			totalRedundance += maxEntropy(word)
+		averageRedundance = totalRedundance/len(words)
+	return averageRedundance
+	
+def averageSentenceLength(txt):
+	sentences = tokenize.sent_tokenize(txt)
+	tokenizer = RegexpTokenizer(r'\w+')
+	averageSentenceLength = 0
+	if len(sentences) > 0:
+		totalSentencesLength = 0
+		for s in sentences:
+			totalSentencesLength += len(tokenizer.tokenize(s))
+		averageSentenceLength = totalSentencesLength/len(sentences) # hier eventuell noch -1, weil Satzende wird immer mitgezählt
+	return averageSentenceLength
+	
+def averageWordlength(txt):
+	tokenizer = RegexpTokenizer(r'\w+')
+	words = tokenizer.tokenize(txt)
+	averageWordlength = 0
+	#print(words)
+	if len(words) > 0:
+		totalWordLength = 0
+		for w in words:
+			totalWordLength += len(w)
+		averageWordlength = totalWordLength/len(words)
+	return averageWordlength
+	
+def fillWords(txt):
+	fillwords = ["er", "uhm", "uh", "ahm", "hm", "ah", "um", "erm", "huh", "oh"]
+	tokenizer = RegexpTokenizer(r'\w+')
+	words = tokenizer.tokenize(txt)
+	totalFillwords = 0
+	for w in words:
+		if w in fillwords:
+			totalFillwords += 1
+	return totalFillwords
+	
+#print(fillWords("Could, ah, you show me, uh, where the caretaker is, hm, working?"))
+#print(averageSentenceLength("Hey! Hal? Hdl."))
+#print(averageWordlength("ha, ha, ha ha"))
+#for k,v in wahrscheinlichkeiten("hallo").items():
+	#print(k + ": " + str(v))
+#print(str(entropy("abcdefghijklmnopqrstuvwxyz")))
+#print(token_type_stats("hallo du \n da da"))
+#print(makeEverythingRight("Mulder and Scully make their way through the crowded bar. We see many people sitting at the bar, but one particular man seems to be watching Mulder and Scully. Scully is looking through a file, we see a photo of a man, the same man we saw trembling in his home at the start, in a military uniform"))
---
+++ singleSeasons/SentenceLength/sorting.py
@@ -0,0 +1,300 @@
+#from folge1 import getFolgenInhalt, isValid
+from metrics import entropy, averageEntropy, token_type_stats, averageRedundance, averageSentenceLength, averageWordlength, fillWords
+# für Staffel - Figurwerte
+# Episode, Figur, Demenz (types/tokens), Entropie, Redundanz
+
+def detNextEpisode(urn):
+	#vielleicht finde ich irgendwann mal einen besseren Weg
+	if isValid(urn):
+		chunks = urn.split(":")
+		i = chunks[-2]
+		if i[-2] == "0" and i[-1] == "9":
+			prev1 = i[-1]
+			prev2 = i[-2] # die Zehnerstelle
+			prevAll = int(i[-2])+int(i[-1])
+			nxt = str(prevAll+1)
+			i = i[:-2]
+			i += str(nxt)
+			print(i)
+		elif i[-2] == "0":
+			prev = i[-1]
+			nxt = int(prev)+1
+			i = i[:-1]
+			print(i)
+			i += str(nxt)
+			print(i)
+		else:
+			prev1 = i[-1]
+			prev2 = i[-2] # die Zehnerstelle
+			prevAll = i[-2]+i[-1]
+			nxt = str(prevAll+1)
+			print(nxt)
+			i = i[:-2]
+			i += str(nxt)
+			print(i)
+		chunks[-2] = i
+		chunks2 = ":".join(chunks)
+		return chunks2
+	else:
+		print("not valid")
+		
+#print(detNextEpisode("urn:cts:xfiles:s1.1X10:1")) die Methode ist zu aufwändig. da geht von Hand eintragen schneller
+
+def mulder():
+ contentMulder = [getFolgenInhalt("urn:cts:xfiles:s1.1X01:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X02:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X03:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X04:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X05:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X06:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X07:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X08:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X09:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X10:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X11:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X12:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X13:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X14:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X15:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X16:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X17:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X18:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X19:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X20:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X21:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X22:1", " MULDER", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X23:1", " MULDER", "")]
+ return contentMulder
+	
+def scully():
+ contentScully = [getFolgenInhalt("urn:cts:xfiles:s1.1X01:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X02:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X03:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X04:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X05:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X06:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X07:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X08:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X09:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X10:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X11:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X12:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X13:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X14:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X15:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X16:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X17:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X18:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X19:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X20:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X21:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X22:1", " SCULLY", "")
+	                ,getFolgenInhalt("urn:cts:xfiles:s1.1X23:1", " SCULLY", "")]
+ return contentScully
+	
+def writeEntropy():
+	f = open("valuesEntropy.csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	m = mulder()
+	s = scully()
+	for i in range(1, 24):
+		start += str(i)+","+str(averageEntropy(m[i-1]))+","+str(averageEntropy(s[i-1]))+"\n"
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeDementia():
+	f = open("valuesDementia.csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	m = mulder()
+	mf = open("MulderS1", "w")
+	mf.write(" ".join(m))
+	mf.flush
+	mf.close
+	s = scully()
+	sf = open("ScullyS1", "w")
+	sf.write(" ".join(s))
+	sf.flush
+	sf.close
+	for i in range(1, 24):
+		wordsM = m[i-1].split()
+		wordsS = s[i-1].split()
+		start += "Episode_"+str(i)+","+str(token_type_stats(wordsM))+","+str(token_type_stats(wordsS))+"\n"
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeEntropy2(staffeln, filename):
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	#m = mulder()
+	#s = scully()
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			for i in range(0, episodenzahlen[z]):
+				print(episodenzahlen[z])
+				m = open("toLower_mulder"+str(z+1), "r")
+				s = open("toLower_scully"+str(z+1), "r")
+				mulder = m.read()
+				mulderEpisodes = mulder.split("separator")
+				print(str(len(mulderEpisodes)))
+				scully = s.read()
+				scullyEpisodes = scully.split("separator")
+				print(str(len(scullyEpisodes)))
+				if ((averageEntropy(mulderEpisodes[i]) != 0) and (averageEntropy(scullyEpisodes[i]) != 0)):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(averageEntropy(mulderEpisodes[i]))+","+str(averageEntropy(scullyEpisodes[i]))+"\n"
+					
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeRedundance(staffeln, filename):
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	#m = mulder()
+	#s = scully()
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			for i in range(0, episodenzahlen[z]):
+				#print(episodenzahlen[z])
+				m = open("toLower_mulder"+str(z+1), "r")
+				s = open("toLower_scully"+str(z+1), "r")
+				mulder = m.read()
+				mulderEpisodes = mulder.split("separator")
+				#print(str(len(mulderEpisodes)))
+				scully = s.read()
+				scullyEpisodes = scully.split("separator")
+				#print(str(len(scullyEpisodes)))
+				if ((averageRedundance(mulderEpisodes[i]) != 0) and (averageRedundance(scullyEpisodes[i]) != 0)):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(averageRedundance(mulderEpisodes[i]))+","+str(averageRedundance(scullyEpisodes[i]))+"\n"
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+
+def writeDementia2(staffeln, filename):
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			m = open("toLower_mulder"+str(z+1), "r")
+			wordsM = m.read()
+			wordsM_Episodes = wordsM.split("separator")
+			print(str(len(wordsM_Episodes)))
+			s = open("toLower_scully"+str(z+1), "r")
+			wordsS = s.read()
+			wordsS_Episodes = wordsS.split("separator")
+			print(str(len(wordsS_Episodes)))
+			for i in range(0, episodenzahlen[z]):
+				if ((token_type_stats(wordsM_Episodes[i].split()) != 0) and (token_type_stats(wordsS_Episodes[i].split()) != 0)):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(token_type_stats(wordsM_Episodes[i].split()))+","+str(token_type_stats(wordsS_Episodes[i].split()))+"\n"
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeSentences(staffeln, filename):
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			m = open("toLower_mulder"+str(z+1), "r")
+			txt = m.read()
+			m_Episodes = txt.split("separator")
+			s = open("toLower_scully"+str(z+1), "r")
+			txtS = s.read()
+			s_Episodes = txtS.split("separator")
+			for i in range(0, episodenzahlen[z]):
+				if ((averageSentenceLength(m_Episodes[i]) != 0) and (averageSentenceLength(s_Episodes[i]) != 0)):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(averageSentenceLength(m_Episodes[i]))+","+str(averageSentenceLength(s_Episodes[i]))+"\n"
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeWordLengths(staffeln, filename):
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			m = open("toLower_mulder"+str(z+1), "r")
+			txt = m.read()
+			m_Episodes = txt.split("separator")
+			s = open("toLower_scully"+str(z+1), "r")
+			txtS = s.read()
+			s_Episodes = txtS.split("separator")
+			for i in range(0, episodenzahlen[z]):
+				if ((averageWordlength(m_Episodes[i]) != 0) and (averageWordlength(s_Episodes[i]) != 0)):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(averageWordlength(m_Episodes[i]))+","+str(averageWordlength(s_Episodes[i]))+"\n"
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+	
+def writeFillwords(staffeln, filename):
+	# leave out 6-19, 2-7, 4-21, 5-1, 5-15, 2-20, 4-7, 5-20
+	missOut = ["S6E19", "S2E7", "S4E21", "S5E1", "S5E15", "S2E20", "S4E7", "S5E20"]
+	f = open(filename+".csv", "w")
+	start = "Episode,MULDER,SCULLY\n"
+	episodenzahlen = [23, 25, 24, 24, 20, 22]
+	z = 0
+	while z < 6:
+		if z in staffeln:
+			m = open("toLower_mulder"+str(z+1), "r")
+			txt = m.read()
+			m_Episodes = txt.split("separator")
+			print(str(len(m_Episodes)))
+			s = open("toLower_scully"+str(z+1), "r")
+			txtS = s.read()
+			s_Episodes = txtS.split("separator")
+			print(str(len(s_Episodes)))
+			for i in range(0, episodenzahlen[z]):
+				if ("S"+str(z+1)+"E"+str(i+1) not in missOut):
+					start += "S"+str(z+1)+"E"+str(i+1)+","+str(fillWords(m_Episodes[i]))+","+str(fillWords(s_Episodes[i]))+"\n"
+				else:
+					pass
+			m.close
+			s.close
+		z += 1
+	f.write(start)
+	f.flush
+	f.close
+
+#writeEntropy2([0,1,2,3,4,5], "valuesEntropyLower")
+#writeEntropy2([5], "valuesEntropySeason6")	
+#writeDementia2([5], "valuesDementiaSeason6")
+#writeRedundance([5], "valuesRedundancySeason6")
+writeSentences([5], "valuesSatzlaengenSeason6")
+#writeWordLengths([0,1,2,3,4,5], "valuesWortlaengenLower")
+#writeFillwords([0,1,2,3,4,5], "valuesFillwordsLower")
